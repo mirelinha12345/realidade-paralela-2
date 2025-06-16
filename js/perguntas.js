c16bbe725e5a5e2ab195cdb7012faa5131110820
mirelinha12345 <141829141+mirelinha12345@users.noreply.github.com>
2025-06-16 12:13:24 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,111 @@
+export const perguntas = [
+    {
+        enunciado: "Você encontra uma IA que pode abrir portais para outras realidades, onde diferentes versões de sua vida e do mundo existem. Você tem a chance de visitar qualquer uma delas. Qual versão de sua vida você escolheria visitar?",
+        alternativas: [
+            {
+                texto: "Visitar um mundo onde fiz escolhas diferentes e ver como minha vida teria sido.",
+                afirmacao: [
+                    "Você se sente ansioso ao pensar em como suas escolhas poderiam ter moldado sua vida, mas logo percebe que algumas decisões passadas talvez não precisassem ser mudadas para alcançar felicidade.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Visitar uma realidade onde minha vida é idêntica à atual, mas em um futuro mais distante.",
+                afirmacao: [
+                    "A sensação de continuidade te acalma, mas você começa a perceber que o futuro não é fixo. Mesmo no 'mundo perfeito', ainda existem dilemas e mudanças imprevisíveis.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Você decide viajar para uma realidade alternativa. Ao chegar, percebe que pequenas mudanças podem ter grandes consequências. Qual aspecto você decide mudar?",
+        alternativas: [
+            {
+                texto: "Fazer uma escolha diferente em uma decisão importante do meu passado.",
+                afirmacao: [
+                    "Alterar o passado parece ser uma solução simples para corrigir erros, mas ao ver as consequências em outras pessoas, você percebe que o destino é mais complexo do que imaginava.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Tentar mudar a história do mundo em uma grande escala, como evitar um desastre global.",
+                afirmacao: [
+                    "Você sente o peso da responsabilidade, mas logo se dá conta de que até as ações mais altruístas podem ter efeitos colaterais inesperados.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Após explorar várias realidades, você começa a questionar se está fazendo a coisa certa. Cada mudança traz consequências inesperadas. Como você lida com essa responsabilidade?",
+        alternativas: [
+            {
+                texto: "Continuo fazendo mudanças, acreditando que posso melhorar o mundo e minha vida.",
+                afirmacao: [
+                    "Seu impulso de melhorar tudo ao seu redor é inabalável, mas você começa a perceber que, em algumas realidades, talvez o 'melhor' não seja o que você pensava.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Decido não mudar mais nada, aceitando que cada realidade tem seu próprio destino.",
+                afirmacao: [
+                    "Você finalmente encontra paz, aceitando que algumas coisas estão além de seu controle. Mas será que você ainda pode ser feliz com o que já está estabelecido?",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao retornar a uma realidade após mudar algo significativo, você percebe que as coisas não saíram como esperava. Algumas pessoas ficaram melhores, mas outras piores. Como você reage?",
+        alternativas: [
+            {
+                texto: "Tento corrigir os erros e fazer tudo certo desta vez.",
+                afirmacao: [
+                    "Você se sente determinado a consertar as coisas, mas logo percebe que nem todos têm as mesmas intenções que você. As linhas entre o certo e o errado começam a se borrar.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Aceito as mudanças e sigo em frente, sabendo que as realidades são imprevisíveis.",
+                afirmacao: [
+                    "A aceitação se torna um peso mais leve, mas você começa a questionar se realmente deveria ter feito mudanças em primeiro lugar.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Após tantas viagens e mudanças, você começa a perceber que, apesar de todas as possibilidades, a realidade original é a única que você pode realmente controlar. O que você decide fazer?",
+        alternativas: [
+            {
+                texto: "Retorno à minha realidade original, aceitando todas as suas imperfeições e desafios.",
+                afirmacao: [
+                    "Ao retornar, você encontra um novo propósito. A beleza está na imperfeição, e você agora vê sua vida com novos olhos.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Continue viajando entre as realidades, buscando algo melhor, sem nunca realmente 'voltar para casa",
+                afirmacao: [
+                    "A busca incessante por algo melhor te impulsiona a continuar explorando. Você se torna um viajante eterno, sempre buscando algo novo, mas talvez, nunca encontrando a verdadeira paz",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
